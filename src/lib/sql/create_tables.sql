fba77d1ed895180357eb27c060930c2d90384ae9
ehunter-usgs <ehunter@usgs.gov>
2015-05-13 14:01:00 -0600

    Create sql statements for adding/dropping the schema

--- src/lib/sql/create_tables.sql
+++ src/lib/sql/create_tables.sql
@@ -2,14 +2,86 @@
 
 /* 
  * Tables:
+ *  - hazard_basis
  *  - design_code
  *  - risk_category
  *  - design_code_risk_category
  *  - site_class
  *  - design_code_site_class
- *  - hazard_basis
  *  - metadata
  *  - region
  *  - data
  */
 
+CREATE TABLE hazard_basis (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  display_order INTEGER
+);
+
+CREATE TABLE design_code (
+  id INTEGER PRIMARY KEY,
+  hazard_basis INTEGER REFERENCES hazard_basis (id),
+  name VARCHAR(255),
+  display_order INTEGER
+);
+
+CREATE TABLE risk_category (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  display_order INTEGER
+);
+
+CREATE TABLE design_code_risk_category (
+  id INTEGER PRIMARY KEY,
+  design_code_id INTEGER REFERENCES design_code (id),
+  risk_category_id INTEGER REFERENCES risk_category (id)
+);
+
+CREATE TABLE site_class (
+  id INTEGER PRIMARY KEY,
+  value VARCHAR(255),
+  name VARCHAR(255),
+  display_order INTEGER
+);
+
+CREATE TABLE design_code_site_class (
+  id INTEGER PRIMARY KEY,
+  design_code_id INTEGER REFERENCES design_code (id),
+  site_class_id INTEGER REFERENCES site_class (id)
+);
+
+CREATE TABLE metadata (
+  id INTEGER PRIMARY KEY,
+  max_direction_ss NUMERIC,
+  max_direction_s1 NUMERIC,
+  percentile_ss NUMERIC,
+  percentile_s1 NUMERIC,
+  deterministic_floor_ss NUMERIC,
+  deterministic_floor_s1 NUMERIC
+);
+
+CREATE TABLE region (
+  id INTEGER PRIMARY KEY,
+  design_code_id INTEGER REFERENCES design_code (id),
+  metadata_id INTEGER REFERENCES metadata (id),
+  name VARCHAR(255),
+  min_latitude NUMERIC,
+  max_latitude NUMERIC,
+  min_longitude NUMERIC,
+  max_longitude NUMERIC,
+  grid_spacing NUMERIC
+);
+
+CREATE TABLE data (
+  id INTEGER PRIMARY KEY,
+  region_id INTEGER REFERENCES region (id),
+  latitude NUMERIC,
+  longitude NUMERIC,
+  mapped_ss NUMERIC,
+  mapped_s1 NUMERIC,
+  crs NUMERIC,
+  cr1 NUMERIC,
+  geomean_ssd NUMERIC,
+  geomean_s1d NUMERIC
+);
--- src/lib/sql/drop_tables.sql
+++ src/lib/sql/drop_tables.sql
@@ -2,13 +2,23 @@
 
 /* 
  * Tables:
- *  - data
- *  - region
- *  - metadata
  *  - hazard_basis
- *  - design_code_site_class
- *  - site_class
- *  - design_code_risk_category
- *  - risk_category
  *  - design_code
+ *  - risk_category
+ *  - design_code_risk_category
+ *  - site_class
+ *  - design_code_site_class
+ *  - metadata
+ *  - region
+ *  - data
  */
+
+DROP TABLE hazard_basis;
+DROP TABLE design_code;
+DROP TABLE risk_category;
+DROP TABLE design_code_risk_category;
+DROP TABLE site_class;
+DROP TABLE design_code_site_class;
+DROP TABLE metadata;
+DROP TABLE region;
+DROP TABLE data;
