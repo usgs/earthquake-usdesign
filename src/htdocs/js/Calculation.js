64d4d7e8d1db9d1dcb934242e6428d050782a1ca
Eric Martinez <emartinez@usgs.gov>
2015-05-20 11:02:59 -0600

    Initial Calculation implementation.

---
+++ src/htdocs/js/Calculation.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var Model = require('mvc/Model'),
+
+    Util = require('util/Util');
+
+
+var _DEFAULTS = {
+  input: {
+    title: 'Untitled Report',
+    latitude: null,
+    longitude: null,
+    design_code: null,
+    risk_category: null,
+    site_class: null
+  },
+
+  output: {
+    metadata: {
+      max_direction_ss: null,
+      max_direction_s1: null,
+
+      percentile_ss: null,
+      percentile_s1: null,
+      percentile_pga: null,
+
+      deterministic_floor_ss: null,
+      deterministic_floor_s1: null,
+      deterministic_floor_pga: null
+    },
+
+    tl: null,
+
+    data: [
+      {
+        latitude: null,
+        longitude: null,
+
+        mapped_ss: null,
+        crs: null,
+        geomean_ssd: null,
+
+        mapped_s1: null,
+        cr1: null,
+        geomean_s1d: null,
+
+        mapped_pga: null,
+        geomean_pgad: null
+      }
+    ]
+  }
+};
+
+var _ID_SEQUENCE = 0;
+
+/**
+ * A Calculation is a representation of what is returned by the web service. It
+ * consists of both the input (request) and output (response) information. See
+ * the _DEFAULTS for more information about what is included in a calculation.
+ *
+ * @param params {Object}
+ *      An object containing the calculation information. See: _DEFAULTS
+ */
+var Calculation = function (params) {
+  var _this,
+      _initialize,
+
+      _generateId;
+
+
+  params = Util.extend({}, _DEFAULTS, params);
+  _this = Model(params);
+
+  /**
+   * @Constructor
+   *
+   * Initializes a new Calculation instance. Ensures the calculation has
+   * an id.
+   */
+  _initialize = function (/*params*/) {
+    var id = _this.get('id');
+
+    // Make sure each Calculation model has an id. This is important since they
+    // will certainly be in a Collection
+    if (typeof id === 'undefined' || id === null) {
+      _this.set({id: _generateId()}, {silent: true});
+    }
+  };
+
+
+  /**
+   * Generates a unique id for the calulation.
+   *
+   */
+  _generateId = function () {
+    return 'Calculation-' + ((new Date()).getTime()) + '-' + (_ID_SEQUENCE++);
+  };
+
+
+  /**
+   * Calls parent destroy method then frees local allocations.
+   *
+   */
+  _this.destroy = Util.compose(_this.destroy, function () {
+    _generateId = null;
+
+    _initialize = null;
+    _this = null;
+  });
+
+
+  _initialize(params);
+  params = null;
+  return _this;
+};
+
+module.exports = Calculation;
