ed84bf8da9bc9627c8c162ee625972f699533949
Eric Martinez <emartinez@usgs.gov>
2015-05-20 10:05:12 -0600

    Enabled database in configuration file.

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -8,11 +8,13 @@ $APP_DIR = $CONFIG['APP_DIR'];
 $DATA_DIR = $CONFIG['DATA_DIR'];
 $MOUNT_PATH = $CONFIG['MOUNT_PATH'];
 
-/* Database stuff, if applicable.  * /
 // Using NO_DB guard allows pages that do not need database connection to use
 // this same configuration file without adding in database overhead.
 if (!(isset($NO_DB) && $NO_DB)) {
   $DB = new PDO($CONFIG['DB_DSN'], $CONFIG['DB_USER'], $CONFIG['DB_PASS']);
   $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  if (isset($CONFIG['DB_SCHEMA'])) {
+    $DB->exec('SET search_path = ' . $CONFIG['DB_SCHEMA'] . ', public');
+  }
 }
-/**/
