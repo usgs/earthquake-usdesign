c5b961f28c413c0826e875b279a59cd7a19cc6cc
Hal Simpson <hasimpson@usgs.gov>
2015-05-28 12:54:10 -0600

    Added documentation to WebServiceAccessor

--- src/htdocs/js/WebServiceAccessor.js
+++ src/htdocs/js/WebServiceAccessor.js
@@ -22,6 +22,12 @@ var WebServiceAccessor = function (params) {
     _url = params.url || _DEFAULTS.url;
   };
 
+  /**
+   * Fetches Usage from usdesign webserver
+   * @return: {object}
+   *          The json object of how to call the url, and
+   *          all usage data.
+   */
   _this.getUsage = function (callback) {
     Xhr.ajax({
       url: _url,
@@ -31,6 +37,17 @@ var WebServiceAccessor = function (params) {
     });
   };
 
+  /**
+   * Fetches Results from usdesign webserver.
+   *
+   * @param calculation {Calculation}
+   *          A Calculation Model with the input parameters set.
+   * @param callback {function}
+   *          The callback function that getResults will call on success.
+   *
+   * @return: {Calculation}
+   *          Calculation Model filled with the input/output data.
+   */
   _this.getResults = function (calculation, callback) {
     Xhr.ajax({
         url: _buildUrl(calculation),
@@ -40,9 +57,24 @@ var WebServiceAccessor = function (params) {
       });
   };
 
+  /**
+   * Update Calculation. Called on success from the ajax call to the webserver.
+   *
+   * @param calculation {Calculation}
+   *         The calculation Model orginally passed to getResults.
+   * @param data {Object}
+   *        The json object passed back by the webserver.
+   *
+   * @return calculation {Calculation}
+   *         Calculation Model filled with the input/output data.
+   *
+   * @throws: error
+   *         if data contains the error message, an error is thrown.
+   */
   _updateCalculation = function (calculation, data) {
     if (data.error !== undefined) {
-      throw new Error('Must specify design_code, site_class, risk_category' +
+      throw new Error(data.error +
+          ' Must specify design_code, site_class, risk_category' +
           'latitude, longitude, and title; to retrive usdesign data.');
     }
     else {
