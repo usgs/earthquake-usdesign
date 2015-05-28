59cd43914351c8b83358f69f36ee6cdfa79248b9
ehunter-usgs <ehunter@usgs.gov>
2015-05-28 15:10:48 -0600

    Add LocationControl, update title on model change

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -7,14 +7,14 @@ var Calculation = require('Calculation'),
 NEHRP2015InputView({
   el: document.querySelector('#input-view'),
   model: Calculation({
-    // mode: 'output',
-    // input: {
-    //   'title': 'My First Report',
-    //   'latitude': 45,
-    //   'longitude': -70.1,
-    //   'design_code': 1,
-    //   'site_class': 2,
-    //   'risk_category': 2
-    // }
+    mode: 'input',
+    input: {
+      'title': 'My First Report',
+      'latitude': 45,
+      'longitude': -70.1,
+      'design_code': 1,
+      'site_class': 2,
+      'risk_category': 2
+    }
   })
 });
