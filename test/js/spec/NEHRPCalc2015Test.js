056c8d9b04d76acea9c8fa245556634ef3caa144
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-08-04 14:59:29 -0600

    updated test to better work with S1D value

--- test/js/spec/NEHRPCalc2015Test.js
+++ test/js/spec/NEHRPCalc2015Test.js
@@ -7,7 +7,7 @@ var NEHRPCalc2015 = require('NEHRPCalc2015'),
 
 
 var expect = chai.expect;
-var EPSILISON = 0.00001;
+var EPSILION = 0.00001;
 
 var calculate = Calculation({
   'input': {
@@ -51,6 +51,51 @@ var calculate = Calculation({
   }
 });
 
+/**
+ *  This calculation is needed to test S1D which requires a different
+ *  deterministic floor value, location, etc.
+ */
+var calculate2 = Calculation({
+  'input': {
+    'title': 'null',
+    'latitude': 34.05,
+    'longitude': -118.25,
+    'design_code': 1,
+    'risk_category': 1,
+    'site_class': 4
+  },
+  'output': {
+  'data': [
+    {
+      'latitude': 34.05,
+      'longitude': -118.25,
+      'mapped_ss': 1.99347,
+      'mapped_s1': 0.600917,
+      'mapped_pga': 0.843573,
+      'crs': 0.89834,
+      'cr1': 0.89758,
+      'geomean_ssd': 1.246042,
+      'geomean_s1d': 0.3358812,
+      'geomean_pgad': 0.5543649
+    }
+  ],
+  'metadata': {
+    'region_name': 'Conterminous US',
+    'region_id': 6,
+    'max_direction_ss': 1.1,
+    'max_direction_s1': 1.3,
+    'percentile_ss': 1.8,
+    'percentile_s1': 1.8,
+    'percentile_pga': 1.8,
+    'deterministic_floor_ss': 1.5,
+    'deterministic_floor_s1': 0.6,
+    'deterministic_floor_pga': 0.5,
+    'grid_spacing': 0.05
+  },
+  'tl': null
+}
+});
+
 var calc = NEHRPCalc2015();
 
 describe('NEHRPCalc2015Test', function () {
@@ -79,20 +124,20 @@ describe('NEHRPCalc2015Test', function () {
 
     it('getSsuh is correct', function () {
       expect(calc.getSsuh(calculate)).to.be.closeTo(
-          0.39930000000000004, EPSILISON);
+          0.39930000000000004, EPSILION);
     });
 
     it('getS1uh is correct', function () {
-      expect(calc.getS1uh(calculate)).to.be.closeTo(0.1833, EPSILISON);
+      expect(calc.getS1uh(calculate)).to.be.closeTo(0.1833, EPSILION);
     });
 
 
     it('getSsur', function () {
-      expect(calc.getSsur(calculate)).to.be.closeTo(0.3489043, EPSILISON);
+      expect(calc.getSsur(calculate)).to.be.closeTo(0.3489043, EPSILION);
     });
 
     it('getS1ur', function () {
-      expect(calc.getS1ur(calculate)).to.be.closeTo(0.15706, EPSILISON);
+      expect(calc.getS1ur(calculate)).to.be.closeTo(0.15706, EPSILION);
     });
 
     it('getSsd', function () {
@@ -100,27 +145,27 @@ describe('NEHRPCalc2015Test', function () {
     });
 
     it('getS1d', function () {
-      expect(calc.getS1d(calculate)).to.equal(0.6);
+      expect(calc.getS1d(calculate2)).to.be.closeTo(0.78596, EPSILION);
     });
 
     it('getFv', function () {
-      expect(calc.getFv(calculate)).to.be.closeTo(2.28586, EPSILISON);
+      expect(calc.getFv(calculate)).to.be.closeTo(2.28586, EPSILION);
     });
 
     it('getSms', function () {
-      expect(calc.getSms(calculate)).to.be.closeTo(0.53064, EPSILISON);
+      expect(calc.getSms(calculate)).to.be.closeTo(0.53064, EPSILION);
     });
 
     it('getSm1', function () {
-      expect(calc.getSm1(calculate)).to.be.closeTo(0.3590395, EPSILISON);
+      expect(calc.getSm1(calculate)).to.be.closeTo(0.3590395, EPSILION);
     });
 
     it('getSds', function () {
-      expect(calc.getSds(calculate)).to.be.closeTo(0.3537602, EPSILISON);
+      expect(calc.getSds(calculate)).to.be.closeTo(0.3537602, EPSILION);
     });
 
     it('getSd1', function () {
-      expect(calc.getSd1(calculate)).to.be.closeTo(0.239359, EPSILISON);
+      expect(calc.getSd1(calculate)).to.be.closeTo(0.239359, EPSILION);
     });
 
     it('getPga', function () {
@@ -141,13 +186,13 @@ describe('NEHRPCalc2015Test', function () {
       spectraArray = calc.getSdSpectra(calculate);
 
       expect(spectraArray[0][0]).to.equal(0);
-      expect(spectraArray[0][1]).to.be.closeTo(0.14150411, EPSILISON);
+      expect(spectraArray[0][1]).to.be.closeTo(0.14150411, EPSILION);
 
-      expect(spectraArray[1][0]).to.be.closeTo(0.13532311, EPSILISON);
-      expect(spectraArray[1][1]).to.be.closeTo(0.35376028, EPSILISON);
+      expect(spectraArray[1][0]).to.be.closeTo(0.13532311, EPSILION);
+      expect(spectraArray[1][1]).to.be.closeTo(0.35376028, EPSILION);
 
-      expect(spectraArray[2][0]).to.be.closeTo(0.67661556, EPSILISON);
-      expect(spectraArray[2][1]).to.be.closeTo(0.35376028, EPSILISON);
+      expect(spectraArray[2][0]).to.be.closeTo(0.67661556, EPSILION);
+      expect(spectraArray[2][1]).to.be.closeTo(0.35376028, EPSILION);
     });
 
     it('getSmSpectra', function () {
@@ -156,13 +201,13 @@ describe('NEHRPCalc2015Test', function () {
       spectraArray = calc.getSmSpectra(calculate);
 
       expect(spectraArray[0][0]).to.equal(0);
-      expect(spectraArray[0][1]).to.be.closeTo(0.212256171, EPSILISON);
+      expect(spectraArray[0][1]).to.be.closeTo(0.212256171, EPSILION);
 
-      expect(spectraArray[1][0]).to.be.closeTo(0.135323113, EPSILISON);
-      expect(spectraArray[1][1]).to.be.closeTo(0.530640429, EPSILISON);
+      expect(spectraArray[1][0]).to.be.closeTo(0.135323113, EPSILION);
+      expect(spectraArray[1][1]).to.be.closeTo(0.530640429, EPSILION);
 
-      expect(spectraArray[2][0]).to.be.closeTo(0.676615569, EPSILISON);
-      expect(spectraArray[2][1]).to.be.closeTo(0.530640429, EPSILISON);
+      expect(spectraArray[2][0]).to.be.closeTo(0.676615569, EPSILION);
+      expect(spectraArray[2][1]).to.be.closeTo(0.530640429, EPSILION);
     });
 
     it('getSiteClass', function () {
@@ -170,19 +215,19 @@ describe('NEHRPCalc2015Test', function () {
     });
 
     it('getSs', function () {
-      expect(calc.getSs(calculate)).to.be.closeTo(0.348904347, EPSILISON);
+      expect(calc.getSs(calculate)).to.be.closeTo(0.348904347, EPSILION);
     });
 
     it('getS1', function () {
-      expect(calc.getS1(calculate)).to.be.closeTo(0.15706977, EPSILISON);
+      expect(calc.getS1(calculate)).to.be.closeTo(0.15706977, EPSILION);
     });
 
     it('getSds', function () {
-      expect(calc.getSds(calculate)).to.be.closeTo(0.35376, EPSILISON);
+      expect(calc.getSds(calculate)).to.be.closeTo(0.35376, EPSILION);
     });
 
     it('getSd1', function () {
-      expect(calc.getSd1(calculate)).to.be.closeTo(0.23935972, EPSILISON);
+      expect(calc.getSd1(calculate)).to.be.closeTo(0.23935972, EPSILION);
     });
 
     it('getPga', function () {
@@ -190,7 +235,7 @@ describe('NEHRPCalc2015Test', function () {
     });
 
     it('getPgam', function () {
-      expect(calc.getPgam(calculate)).to.be.closeTo(0.25920, EPSILISON);
+      expect(calc.getPgam(calculate)).to.be.closeTo(0.25920, EPSILION);
     });
   });
 
@@ -208,7 +253,7 @@ describe('NEHRPCalc2015Test', function () {
         'linearlog',
         function () {
       expect(calc.interpolateValue(3, 2, 1/2, 0, 1, 'linearlog')).to.be.closeTo(
-          2.4494897427, EPSILISON);
+          2.4494897427, EPSILION);
     });
 
     it('interpolateValue throws error when a y value is 0', function () {
