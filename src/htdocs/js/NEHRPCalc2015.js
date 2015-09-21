c6296e96600f05ccc15c3ff79f98a0f67a27430d
Eric Martinez <emartinez@usgs.gov>
2015-09-21 11:34:49 -0600

    Patched PGA computation process.

--- src/htdocs/js/NEHRPCalc2015.js
+++ src/htdocs/js/NEHRPCalc2015.js
@@ -421,9 +421,10 @@ var NEHRPCalc2015 = function (params) {
 
     if (pga === null) {
       metadata = calculation.get('output').get('metadata');
-      probabilisticPga = result.get('mapped_pga');
+      probabilisticPga = result.get('mapped_pga') *
+          metadata.get('percentile_pga');
       deterministicPga = Math.max(result.get('geomean_pgad'),
-          metadata.get('deterministic_flor_pga'));
+          metadata.get('deterministic_floor_pga'));
 
       pga = Math.min(probabilisticPga, deterministicPga);
       result.set({
