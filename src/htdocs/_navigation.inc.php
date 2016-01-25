2772dd674e940c1c3af227f01bc5338bcb087a8e
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-01-25 11:14:11 -0700

    removed mount path

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -3,6 +3,5 @@
 include_once '../conf/config.inc.php';
 
 print navItem($MOUNT_PATH . '/index.php', 'U.S. Seismic Design Maps');
-print navItem($MOUNT_PATH .
-    'https://github.com/usgs/earthquake-usdesign/wiki/US-Seismic-Design-Maps-Application-Documentation',
+print navItem('https://github.com/usgs/earthquake-usdesign/wiki/US-Seismic-Design-Maps-Application-Documentation',
     'Documentation');
