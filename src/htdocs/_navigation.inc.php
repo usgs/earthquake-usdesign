607011d96e32d5e590c5603772b75af2d2caff8f
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-01-25 11:06:56 -0700

    Added documentation link to left navigation

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -3,3 +3,6 @@
 include_once '../conf/config.inc.php';
 
 print navItem($MOUNT_PATH . '/index.php', 'U.S. Seismic Design Maps');
+print navItem($MOUNT_PATH .
+    'https://github.com/usgs/earthquake-usdesign/wiki/US-Seismic-Design-Maps-Application-Documentation',
+    'Documentation');
