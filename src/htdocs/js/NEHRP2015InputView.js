d145819d1fc2b3ae4a57ec02e87a41a5f5672137
Eric Martinez <emartinez@usgs.gov>
2016-02-02 13:04:33 -0700

    Using no confidence as fallback.

--- src/htdocs/js/NEHRP2015InputView.js
+++ src/htdocs/js/NEHRP2015InputView.js
@@ -566,7 +566,7 @@ var NEHRP2015InputView = function (params) {
         confidence = location.confidence ||
             _locationControlInput.getLocation().confidence;
       } catch (e) {
-        confidence = ConfidenceCalculator.BELOW_AVERAGE_CONFIDENCE;
+        confidence = ConfidenceCalculator.NO_CONFIDENCE;
       }
 
       _locationControlInput.setLocation({
